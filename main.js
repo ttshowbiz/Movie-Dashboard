8f224877200f1fd36a1cea38aa000a735604ca1c
Savannah Pendergrass <sav.penny+github@gmail.com>
2023-02-08 20:21:11 -0800

    Quick push

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Send the response body as "Hello World"
-    response.end('Now Playing\n');
- }).listen(8081);
- 
- // Console will print the message
- console.log('Server running at http://127.0.0.1:8081/');
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res, next) {
+  res.render('index', { pagetitle: 'Movie Dashboard' });
+});
+module.exports = router;
