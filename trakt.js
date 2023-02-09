15351d3571ae5d6b2921dd6701379858071393d0
Savannah Pendergrass <sav.penny+github@gmail.com>
2023-02-08 20:09:49 -0800

    Converting to nodejs

--- trakt.js
+++ trakt.js
@@ -1,28 +1,19 @@
-var initialized = false;
-const Trakt = require('trakt.tv')
+const fs = require('fs')
 
-function initialize() {
-    if(initialized)
-        return
-
-    let options = {
-        client_id: 'b1558996f64a6bec213cd24b70822576a87462b400d31b548301e86533372a56',
-        client_secret: 'e1b7eb3a848c57f6ab7871f9ac8758605252f1a638bd712218ce579e906cc79b',
-        redirect_uri: null,   // defaults to 'urn:ietf:wg:oauth:2.0:oob'
-        api_url: null,        // defaults to 'https://api.trakt.tv'
-        useragent: null,      // defaults to 'trakt.tv/<version>'
-        pagination: true      // defaults to false, global pagination (see below)
-      }
-      const trakt = new Trakt(options)
-      const traktAuthUrl = trakt.get_url()
-      
-      trakt.exchange_code('code', 'csrf token (state)').then(result => {})
-
-      initialized = true
+class Trakt {
+    constructor(info_file) {
+        fs.readFile(info_file, 'utf8', (err, jsonString) => {
+            if (err) {
+                console.log("File read failed:", err)
+                return
+            }
+            console.log('File data:', jsonString)
+        })
+    }
 }
 
+var trakt = new Trakt('./trackt_info.json')
+
 function get_now_playing() {
-    initialize()
-    var now_playing = trakt.movies.watching()
-    console.log("Now Playing: " + now_playing)
+    console.log('temp');
 }
